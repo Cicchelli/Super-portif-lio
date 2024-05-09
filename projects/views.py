7716c71bda0de7854217c937a85a5872879bed2a
Stephan <cicchelli.stephan@gmail.com>
2024-05-09 14:35:19 -0300

    Req 2

---
+++ projects/views.py
@@ -0,0 +1,44 @@
+from rest_framework import viewsets
+from .models import Profile, Project, CertifyingInstitution, Certificate
+from .serializers import (
+    CertificateSerializer,
+    CertifyingInstitutionSerializer,
+    ProfileSerializer,
+    ProjectSerializer,
+)
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.shortcuts import render
+
+
+class ProfileViewSet(viewsets.ModelViewSet):
+    queryset = Profile.objects.all()
+    serializer_class = ProfileSerializer
+
+    def get_permissions(self):
+        if self.request.method == "GET":
+            return [AllowAny()]
+        return [IsAuthenticated()]
+
+    def retrieve(self, request, *args, **kwargs):
+        if request.method == "GET":
+            profile = Profile.objects.get(id=kwargs["pk"])
+
+            context = {"profile": profile, "projects": profile.projects.all()}
+
+            return render(request, "profile_detail.html", context)
+        return super().retrieve(request, *args, **kwargs)
+
+
+class ProjectViewSet(viewsets.ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+
+
+class CertifyingInstitutionViewSet(viewsets.ModelViewSet):
+    queryset = CertifyingInstitution.objects.all()
+    serializer_class = CertifyingInstitutionSerializer
+
+
+class CertificateViewSet(viewsets.ModelViewSet):
+    queryset = Certificate.objects.all()
+    serializer_class = CertificateSerializer
